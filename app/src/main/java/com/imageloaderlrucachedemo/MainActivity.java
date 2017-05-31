8151129e0b4cb18ed3f64a344b22e5885d32f436
qingchen <19655910@qq.com>
2017-05-31 15:08:01 +0800

    新增

--- app/src/main/java/com/imageloaderlrucachedemo/MainActivity.java
+++ app/src/main/java/com/imageloaderlrucachedemo/MainActivity.java
@@ -7,10 +7,11 @@ import android.widget.ImageView;
 import com.imageloaderlrucachedemo.Utils.ImageLoader;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView mIv1, mIv2, mIv3;
+    private ImageView mIv1, mIv2, mIv3, mIv4;
     private ImageLoader mImageLoader;
     private String[] urls = {"https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2056231335,4212597522&fm=23&gp=0.jpg",
-            "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3071269338,4132538973&fm=11&gp=0.jpg", "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4033006514,1344445694&fm=11&gp=0.jpg"};//测试数据
+            "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3071269338,4132538973&fm=11&gp=0.jpg",
+            "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4033006514,1344445694&fm=11&gp=0.jpg", "http://img3.imgtn.bdimg.com/it/u=3199951610,2733797063&fm=23&gp=0.jpg"};//测试数据
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         mIv1 = (ImageView) findViewById(R.id.img1);
         mIv2 = (ImageView) findViewById(R.id.img2);
         mIv3 = (ImageView) findViewById(R.id.img3);
+        mIv4 = (ImageView) findViewById(R.id.img4);
 
     }
 
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         mImageLoader.displayImage(urls[0], mIv1);
         mImageLoader.displayImage(urls[1], mIv2);
         mImageLoader.displayImage(urls[2], mIv3);
+        mImageLoader.displayImage(urls[3], mIv4);
 
     }
 }
